e26465d1e4ea799e4b7475206389fda572243ff9
feshchenko <pavel.feschenko@gmail.com>
2022-10-07 13:33:09 +0300

    feshchenko/feature/ basic autocomplete ipmlementation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css';
 import AutoComplete from '../components/AutoComplete';
 
 const Home: NextPage = () => {
+  const [userSelection, setUserSelection] = React.useState<string>('');
+  const onItemClick = React.useCallback((item: string) => {
+    setUserSelection(item);
+  }, []);
+
   return (
     <div className={styles.container}>
-      <AutoComplete />
-      <div className={styles.userSelection}>USER SELECTION HERE</div>
+      <AutoComplete onItemClick={onItemClick} />
+      <div className={styles.userSelection}>USER SELECTION HERE {userSelection || ''}</div>
     </div>
   )
 }
