f172a223c8e94c41208374184870a9409b6dc404
eladr7 <eladr7@gmail.com>
2023-07-11 18:37:53 +0300

    Implemented AutoComplete with delayed respones handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css';
-import AutoComplete from '../components/AutoComplete';
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import AutoComplete from "../components/AutoComplete";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [selectedSuggestion, setSelectedSuggestion] = useState<string>("");
   return (
     <div className={styles.container}>
-      <AutoComplete />
-      <div className={styles.userSelection}>USER SELECTION HERE</div>
+      <AutoComplete setSelectedSuggestion={setSelectedSuggestion} />
+      <div className={styles.userSelection}>{selectedSuggestion}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
